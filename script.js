79cc0e227deb5a9942e55bbee59f2f0ff5c5e232
Jack Barratt <barratt_jack@yahoo.com>
2021-03-04 18:19:10 +0000

    Update script.js
    adjustments to creating book - still very much a wip

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+const newAddedBook = document.createElement("div");
+
 document.getElementById("addbook").addEventListener("click", () => {
-    document.getElementById("popup-form").style.display = "flex";
+  document.getElementById("popup-form").style.display = "flex";
 })
 
 document.querySelector(".close").addEventListener("click", () => {
-    document.getElementById("popup-form").style.display = "none";
+  document.getElementById("popup-form").style.display = "none";
 })
 
 document.querySelector(".add-to-library").addEventListener("click", () => {
-    document.getElementById("popup-form").style.display = "none";
-    addBookToLibrary();
+  document.getElementById("popup-form").style.display = "none";
+  addBookToLibrary();
 })
 
 // document.querySelector(".delete-book").addEventListener("click", () => {
@@ -19,33 +21,51 @@ document.querySelector(".add-to-library").addEventListener("click", () => {
 let myLibrary = [];
 
 function Book(bookTitle, bookAuthor, bookPages) {
-    this.book = bookTitle;
-    this.author = bookAuthor;
-    this.pages = bookPages;
+  this.book = bookTitle;
+  this.author = bookAuthor;
+  this.pages = bookPages;
 }
 
 function addBookToLibrary() {
-    const title = document.querySelector(".book-title").value
-    const author = document.querySelector(".author").value
-    const pages = document.querySelector(".pages").value
+  const title = document.querySelector(".book-title").value
+  const author = document.querySelector(".author").value
+  const pages = document.querySelector(".pages").value
 
-    myLibrary.push(
-        new Book(title, author, pages)
-        );
+  myLibrary.push(
+      new Book(title, author, pages)
+      );
 
-    const bookContainer = document.getElementById("book-container");
-    const newDiv = document.createElement("div");
-    bookContainer.appendChild(newDiv);
-    newDiv.classList.add("new-book");
-    newDiv.appendChild(newDiv);
+  //make div and add class new-book to it
+  const bookContainer = document.getElementById("book-container");
+  const newAddedBook = document.createElement("div");
+  bookContainer.appendChild(newAddedBook);
+  newAddedBook.classList.add("new-book");
+  
+  //make div on new book and add class deletebook to it
+  const deleteBook = document.createElement("div");
+  newAddedBook.appendChild(deleteBook);
+  deleteBook.classList.add("delete-book");
+  deleteBook.innerHTML = "+";
+  
+  //make div on new book and add class title to it
+  const titleOfBook = document.createElement("div");
+  newAddedBook.appendChild(titleOfBook);
+  titleOfBook.classList.add("title");
+  titleOfBook.innerHTML = title;
+  
+  //make div on new book and add class book-headers to it
+  const authorAndPages = document.createElement("div");
+  newAddedBook.appendChild(authorAndPages);
+  authorAndPages.classList.add("book-headers");
+  authorAndPages.innerHTML = `${author}<br>${pages} pages`
 }
 
 function showLibrary(myLibrary) {
-    for(let i = 0; i < myLibrary.length; i++) {
-        let newDiv = document.createElement("div");
-        let newBook = document.createTextNode(myLibrary[i]);
-        bookContainer.appendChild(newDiv);
-        newDiv.appendChild(newBook);
-        newDiv.classList.add("new-book");
-    }
+  for(let i = 0; i < myLibrary.length; i++) {
+      let newDiv = document.createElement("div");
+      let newBook = document.createTextNode(myLibrary[i]);
+      bookContainer.appendChild(newDiv);
+      newDiv.appendChild(newBook);
+      newDiv.classList.add("new-book");
+  }
 }
