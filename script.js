dc5163be5bcbe79d1d4fc92a50d0f2c16bf53144
Jack Barratt <barratt_jack@yahoo.com>
2021-03-10 10:53:16 +0000

    Update script.js
    cleaned up code and removed some debug properties that were no longer needed

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let bookContainer = document.getElementById("book-container");
 const noBooksDisplay = document.querySelector(".no-books-added");
-// let provider = new firebase.auth.GoogleAuthProvider();
-const signIn = document.getElementById("signin");
 
 let myLibrary = [];
 updateLibrary();
@@ -19,13 +17,6 @@ document.querySelector(".add-to-library").addEventListener("click", () => {
   addBookToLibrary();
 })
 
-const removeBook = document.querySelectorAll(".delete-book");
-removeBook.forEach((book) => {
-  book.addEventListener("click", (e) => {
-      console.dir(e.target)
-  })
-})
-
 function Book(bookTitle, bookAuthor, bookPages) {
   this.title = bookTitle;
   this.author = bookAuthor;
@@ -41,7 +32,6 @@ function addBookToLibrary() {
   myLibrary.push(
       new Book(title, author, pages)
       );
-  
   updateLibrary();
 }
 
@@ -54,8 +44,11 @@ function updateLibrary(){
   let html = "";
   myLibrary.map((book, index) => {
     html += `<div class="new-book">`;
-    // html += `<span class="read">Read</span><input class="read-status" type="checkbox" onclick="myLibrary[${index}].read()" name="">`
-    html += `<span class="read" onclick="myLibrary[${index}].read()">READ</span>`;
+    if (book.readStatus === true) {
+      html += `<span class="read" data-index="${index}" onclick="read(this)">READ</span>`;
+    } else {
+      html += `<span class="read notread" data-index="${index}" onclick="read(this)">READ</span>`;
+    }
     html += `<div class="delete-book" data-index="${index}" onclick="deleteBook(this)">+</div>`;
     html += `<div class="title">${book.title}</div>`;
     html += `<div class="book-headers">${book.author}<br>${book.pages} pages</div></div>`;
@@ -65,61 +58,14 @@ function updateLibrary(){
 
 function deleteBook(e) {
   myLibrary.splice(e.dataset.index, 1);
-  console.log(myLibrary.length);
   updateLibrary();
 }
 
-// Book.prototype.read = function() {
-//   if(!this.readStatus) {
-//     document.querySelectorAll(".read-status").forEach((checkbox) => {
-//       checked = true;
-//       this.readStatus = true;
-//     })
-//   } else {
-//     document.querySelectorAll(".read-status").forEach((checkbox) => {
-//       checked = false;
-//       this.readStatus = false;
-//     })
-//   }
-// }
-
-Book.prototype.read = function() {
-    if(!this.readStatus) {
-      document.querySelectorAll(".read-status").forEach((readButton) => {
-        readButton.classList.add("read.active")
-        this.readStatus = true;
-        console.log(this.readStatus);
-      })
-    } else {
-      document.querySelectorAll(".read-status").forEach((readButton) => {
-        readButton.classList.remove("read.active")
-        this.readStatus = false;
-        console.log(this.readStatus);
-      })
-    }
+function read(e) {
+  if(!myLibrary[e.dataset.index].readStatus){
+    myLibrary[e.dataset.index].readStatus = true;
+  } else {
+    myLibrary[e.dataset.index].readStatus = false;
   }
-
-  // signIn.addEventListener("click", () => {
-  //   firebase.auth()
-  //   .signInWithPopup(provider)
-  //   .then((result) => {
-  //     /** @type {firebase.auth.OAuthCredential} */
-  //     var credential = result.credential;
-  
-  //     // This gives you a Google Access Token. You can use it to access the Google API.
-  //     var token = credential.accessToken;
-  //     // The signed-in user info.
-  //     var user = result.user;
-  //     // ...
-  //   }).catch((error) => {
-  //     // Handle Errors here.
-  //     var errorCode = error.code;
-  //     var errorMessage = error.message;
-  //     // The email of the user's account used.
-  //     var email = error.email;
-  //     // The firebase.auth.AuthCredential type that was used.
-  //     var credential = error.credential;
-  //     // ...
-  //   });
-  
-  // })
+updateLibrary();
+}
